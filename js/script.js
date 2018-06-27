a5974e2959f0f7cf9f893feef76a129e938d85fd
Nathan Jensby <njensby@gmail.com>
2018-06-27 14:29:50 -0500

    refactor and comment

--- js/script.js
+++ js/script.js
@@ -1,173 +1,150 @@
-//event handler for player clicking a square
+// event handler for player clicking a square
 $(document).ready(function() {
   $('input.reset').on('click', clearBoard);
-  //markX or markO depending on whose turn it is
+
+  // markX or markO depending on whose turn it is
   $('.board td').on('click', goHere);
-  //check to see if x wins
-  $('.board td').on('click', checkForX);
-  //check to see if o wins
-  $('.board td').on('click', checkForO);
+
+  // easter eggs
   $('h1 span').on('mouseover', toe);
   $('h1 span').on('mouseleave', robot);
 });
 
+// global variables
 var clickCount = 0;
 var winner = false;
 
+// function called every time a square is clicked.
 function goHere(e) {
   e.preventDefault();
-  if (winner) {
-    // do nothing
-  } else {
+
+  if (!winner) {
     winner = false;
     var square = $(this);
+
+    // if square is already occupied, alert user
     if (square.hasClass('player1') || square.hasClass('player2')) {
       alert("Seat's taken!");
+
+      // if it's player 1's turn
     } else if ($('h2 span').text() === '1') {
+
+      // add class player1 and data player x to square
       square.addClass('player1').data('player', 'x');
+
+      // fill square
       square.text('X');
+
+      // increase click count
       clickCount++;
+
+      // check to see if X won
+      checkFor('x');
+
+      // change player's turn to player 2
       $('h2 span').text('2');
+
+      // if it's player 2's turn
     } else if ($('h2 span').text() === '2') {
+
+      // add class player 2 and data player o to square
       square.addClass('player2').data('player', 'o');
+
+      // fill square
       square.text('O');
+
+      // increase click count
       clickCount++;
+
+      // check to see if O won
+      checkFor('o');
+
+      // change player's turn to player 1
       $('h2 span').text('1');
     }
   }
 }
 
-function checkForX() {
-  if (winner) {
-    // do nothing
-  } else {
+
+function checkFor(player) {
+  var className = player === 'x' ? 'player1' : 'player2';
+
+  if (!winner) {
+    // check to see if any vertical row has a winner
     for (var i = 0; i < 3; i++) {
-      if (
-        $('#row1 td')
-          .eq(i)
-          .data('player') === 'x' &&
-        $('#row2 td')
-          .eq(i)
-          .data('player') === 'x' &&
-        $('#row3 td')
-          .eq(i)
-          .data('player') === 'x'
+      if(
+        $('#row1 td').eq(i).data('player') === player
+          && $('#row2 td').eq(i).data('player') === player
+          && $('#row3 td').eq(i).data('player') === player
       ) {
-        player1Wins();
+        playerWins(player);
       }
     }
 
+    // check to see if any horizontal row has a winner
     if (
-      $('#row1 td.player1').length === 3 ||
-      $('#row2 td.player1').length === 3 ||
-      $('#row3 td.player1').length === 3
+      $(`#row1 td.${className}`).length === 3 ||
+      $(`#row2 td.${className}`).length === 3 ||
+      $(`#row3 td.${className}`).length === 3
     ) {
-      player1Wins();
-    } else if (
-      $('#row1 td')
-        .eq(0)
-        .data('player') === 'x' &&
-      $('#row2 td')
-        .eq(1)
-        .data('player') === 'x' &&
-      $('#row3 td')
-        .eq(2)
-        .data('player') === 'x'
-    ) {
-      player1Wins();
-    } else if (
-      $('#row1 td')
-        .eq(2)
-        .data('player') === 'x' &&
-      $('#row2 td')
-        .eq(1)
-        .data('player') === 'x' &&
-      $('#row3 td')
-        .eq(0)
-        .data('player') === 'x'
-    ) {
-      player1Wins();
-    }
-    if (winner === false && clickCount === 9 && $('h2 span').text() === '2') {
-      catGame();
-    }
-  }
-}
+      playerWins(player);
 
-function checkForO() {
-  if (winner) {
-    // do nothing
-  } else {
-    for (var i = 0; i < 3; i++) {
-      if (
-        $('#row1 td')
-          .eq(i)
-          .data('player') === 'o' &&
-        $('#row2 td')
-          .eq(i)
-          .data('player') === 'o' &&
-        $('#row3 td')
-          .eq(i)
-          .data('player') === 'o'
-      ) {
-        player2Wins();
-      }
+    // check to see if top left to bottom right diagonal has a winner
+    } else if (
+      $('#row1 td')
+        .eq(0)
+        .data('player') === player &&
+      $('#row2 td')
+        .eq(1)
+        .data('player') === player &&
+      $('#row3 td')
+        .eq(2)
+        .data('player') === player
+    ) {
+      playerWins(player);
+
+    // check to see if bottom left to top right diagonal has a winner
+    } else if (
+      $('#row1 td')
+        .eq(2)
+        .data('player') === player &&
+      $('#row2 td')
+        .eq(1)
+        .data('player') === player &&
+      $('#row3 td')
+        .eq(0)
+        .data('player') === player
+    ) {
+      playerWins(player);
     }
 
-    if (
-      $('#row1 td.player2').length === 3 ||
-      $('#row2 td.player2').length === 3 ||
-      $('#row3 td.player2').length === 3
-    ) {
-      player2Wins();
-    } else if (
-      $('#row1 td')
-        .eq(0)
-        .data('player') === 'o' &&
-      $('#row2 td')
-        .eq(1)
-        .data('player') === 'o' &&
-      $('#row3 td')
-        .eq(2)
-        .data('player') === 'o'
-    ) {
-      player2Wins();
-    } else if (
-      $('#row1 td')
-        .eq(2)
-        .data('player') === 'o' &&
-      $('#row2 td')
-        .eq(1)
-        .data('player') === 'o' &&
-      $('#row3 td')
-        .eq(0)
-        .data('player') === 'o'
-    ) {
-      player2Wins();
-    }
+    // if there have been nine clicks and no winner, cat's game
     if (winner === false && clickCount === 9) {
       catGame();
     }
   }
 }
 
-function player1Wins() {
-  var xScore = +$('#p1Score').text();
-  xScore++;
-  $('#p1Score').html(xScore);
-  $('.content').append("<h1 class='winTag'>Player 1 Wins!</h1>");
-  winner = true;
-}
+function playerWins(player) {
+  // figure out which player's score to increment
+  var playerToTarget = player === 'x' ? 'p1Score' : 'p2Score';
 
-function player2Wins() {
-  var oScore = +$('#p2Score').text();
-  oScore++;
-  $('#p2Score').html(oScore);
-  $('.content').append("<h1 class='winTag'>Player 2 Wins!</h1>");
+  // target score and turn into a number
+  var scoreToIncrement = +$(`#${playerToTarget}`).text();
+
+  // increment that player's score
+  scoreToIncrement++;
+
+  // update dom to reflect new score
+  $(`#${playerToTarget}`).html(scoreToIncrement);
+
+  // show that player won
+  $('.content').append(`<h1 class='winTag'>Player ${player === 'x' ? '1' : '2'} Wins!</h1>`)
   winner = true;
 }
 
 function catGame() {
+  // show cat's game
   $('.content').append("<h1 class='winTag'>Cat's Game!</h1>");
   winner = true;
 }
